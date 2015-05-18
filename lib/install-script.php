38ee4912d777294abfe2b619d639f474078878dd
contact-banker <contact-banker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-18 04:11:19 +0000

    New Version Release
    git-svn-id: https://plugins.svn.wordpress.org/wp-review-bank/tags/1.14@1162431 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/install-script.php
@@ -0,0 +1,52 @@
+<?php
+if(!function_exists("create_table_review"))
+{
+	function create_table_review()
+	{
+		$sql= " CREATE TABLE ".wp_review_tbl." (
+			id INTEGER(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+			review_type VARCHAR(100) NOT NULL,
+			heading VARCHAR(100) NOT NULL,
+			description TEXT NOT NULL,
+			review_color VARCHAR(50),
+			font_color VARCHAR(50),
+			heading_background_color VARCHAR(50),
+			background_color VARCHAR(50),
+			body_color VARCHAR(50),
+			minimum_val INTEGER(11),
+			maximum_val INTEGER(11),
+			total DECIMAL(10,2) NOT NULL,
+			no_of_features INTEGER(10) NOT NULL,
+			PRIMARY KEY(id)
+			) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci";
+		dbDelta($sql);
+	}
+}
+if(!function_exists("create_table_features"))
+{
+	function create_table_features()
+	{
+		$sql= " CREATE TABLE ".wp_review_tbl_features." (
+			id INTEGER(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+			review_id INTEGER(10) NOT NULL,
+			feature TEXT NOT NULL,
+			points DECIMAL(10,2) NOT NULL,
+			dynamic_id INTEGER(10) NOT NULL,
+			feature_sorting INTEGER(10) NOT NULL,
+			PRIMARY KEY(id)
+			) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE utf8_general_ci";
+		dbDelta($sql);
+	}
+}
+
+global $wpdb;
+require_once(ABSPATH . "wp-admin/includes/upgrade.php");
+if (count($wpdb->get_var("SHOW TABLES LIKE '" .wp_review_tbl."'")) == 0)
+{
+	create_table_review();
+}
+if(count($wpdb->get_var("SHOW TABLES LIKE '" .wp_review_tbl_features."'")) == 0)
+{
+	create_table_features();
+}
+update_option("review-bank-automatic-update",1);
